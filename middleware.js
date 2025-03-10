48d3dd36e2b5383a3df434f3967a390040ae1a01
SufiDevelope <muhammadsufyan112011@gmail.com>
2025-03-10 03:21:58 -0700

    commented middlewaere

--- middleware.js
+++ middleware.js
@@ -1,41 +1,41 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+// import { withAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    console.log("Middleware triggered for:", req.nextUrl.pathname);
+// export default withAuth(
+//   function middleware(req) {
+//     console.log("Middleware triggered for:", req.nextUrl.pathname);
 
-    const path = req.nextUrl.pathname;
-    const isAuthenticated = !!req.nextauth.token;
+//     const path = req.nextUrl.pathname;
+//     const isAuthenticated = !!req.nextauth.token;
 
-    if (
-      [
-        // "/login",
-        // "/signup",
-        // "/api/auth",
-        // "/api/register",
-        // "/datadeletion",
-        // "/reset-password",
-      ].some((prefix) => path.startsWith(prefix))
-    ) {
-      if (isAuthenticated) {
-        return NextResponse.redirect(new URL("/", req.url));
-      }
-      return null;
-    }
+//     if (
+//       [
+//         // "/login",
+//         // "/signup",
+//         // "/api/auth",
+//         // "/api/register",
+//         // "/datadeletion",
+//         // "/reset-password",
+//       ].some((prefix) => path.startsWith(prefix))
+//     ) {
+//       if (isAuthenticated) {
+//         return NextResponse.redirect(new URL("/", req.url));
+//       }
+//       return null;
+//     }
 
-    if (!isAuthenticated) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-    return null;
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => true,
-    },
-  }
-);
+//     if (!isAuthenticated) {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
+//     return null;
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => true,
+//     },
+//   }
+// );
 
-export const config = {
-  matcher: ["/((?!api/|_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api/|_next/static|_next/image|favicon.ico).*)"],
+// };
